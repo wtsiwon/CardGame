45d1fe421edd4371fd801a840b32cc2853e2fa53
wtsiwon <siwon4727@gmail.com>
2023-04-22 18:00:37 +0900

    CardGame

--- Assets/Script/Card.cs
+++ Assets/Script/Card.cs
@@ -2,17 +2,73 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Card : MonoBehaviour
+[System.Serializable]
+public class PRS
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Vector3 pos;
+    public Quaternion rot;
+    public Vector3 scale;
 
-    // Update is called once per frame
-    void Update()
+    public PRS(Vector3 pos, Quaternion rot, Vector3 scale)
     {
-        
+        this.pos = pos;
+        this.rot = rot;
+        this.scale = scale;
     }
 }
+public class Card : MonoBehaviour
+{
+    public PRS originePRS;
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+
+    }
+
+    public void MoveTransform(PRS prs, bool useLerp, float lerpTime = 0)
+    {
+        if (useLerp == false)
+        {
+            transform.position = prs.pos;
+            transform.rotation = prs.rot;
+            transform.localScale = prs.scale;
+        }
+    }
+
+    public void DoMove( Vector3 targetPos, float time)
+    {
+
+        IEnumerator IDoMove(Vector3 targetPos, float time)
+        {
+            float current = 0;
+            float percent = 0;
+            Vector3 pos = transform.position;
+
+            Vector3 startPos = transform.position;
+            Vector3 endPos = targetPos;
+
+
+            while(percent < 1)
+            {
+                current += Time.deltaTime;
+                percent = current / time;
+                pos = Vector3.Lerp(startPos, endPos, percent);
+
+                transform.position = pos;
+                yield return null;
+            }
+
+            yield break;
+        }
+    }
+
+
+
+}
+
+
--- Assets/Script/CardManager.cs
+++ Assets/Script/CardManager.cs
@@ -4,15 +4,53 @@ using UnityEngine;
 
 public class CardManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Transform cardParent;
+
+    public List<Card> cardList = new List<Card>(10);
+
+    public Card originCard;
+
+    private int cardCount;
+
+    public int CardCount 
+    {
+        get => cardCount;
+        set
+        {
+            cardCount = value;
+
+            //정렬 함수
+        }
+    }
     void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        InputKey();
+    }
+
+    private void InputKey()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            Card card = Instantiate(originCard, cardParent);
+            cardList.Add(card);
+        }
+    }
+
+    private void AddCard()
+    {
+        Card card = Instantiate(originCard, cardParent);
+        cardList.Add(card);
+        CardCount++;
+    }
+
+    private void CardPositionSort()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
